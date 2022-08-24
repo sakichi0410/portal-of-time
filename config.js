2f5d973424fb59254c1345d265e680cc9c7a90a0
Arai000 <r.arai@g.dreamcareer.co.jp>
2022-08-24 11:54:45 +0900

    node.js形式のファイル構造に変更

---
+++ config.js
@@ -0,0 +1,9 @@
+const dbSetting = {
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  port: process.env.port,
+};
+
+exports.dbSetting = dbSetting;
