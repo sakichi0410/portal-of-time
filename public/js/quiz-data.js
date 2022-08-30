433aee4e75113f5da9e0188ed70fb68747d94b9b
daiki-k18 <daikikotetu1218@icloud.com>
2022-08-30 17:12:54 +0900

    koshimo:quiz途中まで

---
+++ public/js/quiz-data.js
@@ -0,0 +1,12 @@
+const quizDataJp = [
+  {
+    question: '大阪冬の陣において、豊富方として参戦し、徳川方の攻撃を出城を築き防いだ武将は誰か？',
+    a: '真田幸村',
+    b: '伊達政宗',
+    c: '毛利元就',
+    d: '石田三成',
+    correct: 'a'
+  }
+];
+
+export { quizDataJp }
