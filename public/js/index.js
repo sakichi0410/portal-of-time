afd330985503110bed3953fd31b01e546cb2d9c1
Arai000 <r.arai@g.dreamcareer.co.jp>
2022-09-15 16:59:05 +0900

    httpPostwo

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,21 @@ const httpGet = async function (url) {
   }
 };
 
-export { httpGet }
+const httpPost = async function (url, data) {
+  console.log("index.js httpPost 1");
+  try {
+    const response = await fetch(url, {
+      method: "POST", // POST
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log("index.js httpPost 2");
+    return response.json(); // JSON のレスポンスを JavaScript のオブジェクトに変換
+  } catch (err) {
+    //console.log(err);
+  }
+};
+
+export { httpGet ,httpPost}
